eeb0424749ebe7fa742e85e8f05be64e6523c9dc
alexshe <alexshe@wix.com>
2022-06-28 15:54:15 +0300

    feat: dedicated machine to run mac builds

--- src/create-matrix.ts
+++ src/create-matrix.ts
@@ -1,12 +1,24 @@
 import * as core from "@actions/core";
 
-const ALL_PLATFORMS = 'Ubuntu-latest","Windows-latest","MacOS-latest';
+const ALL_PLATFORMS = 'ubuntu-latest","windows-electron","macos-latest';
 
 async function run(): Promise<void> {
   try {
-    let platform = core.getInput("os");
-    platform =
-      platform.toLowerCase() == "all" ? ALL_PLATFORMS : `${platform}-latest`;
+    let platform = core.getInput("os");    
+    switch(platform.toLowerCase()) { 
+      case "all": { 
+          platform = ALL_PLATFORMS
+          break; 
+      } 
+      case "windows": { 
+          platform = "windows-electron"
+          break; 
+      } 
+      default: { 
+          platform = `${platform}-latest`
+          break; 
+      } 
+    }   
     const platformJSON = `\"platform\":[\"${platform}\"]`;
     const matrixJSON = `{${platformJSON}}`;
     core.setOutput("matrix", matrixJSON);
